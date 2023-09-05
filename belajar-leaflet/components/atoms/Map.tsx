cc6759d6994af17a3bd01fd46196f43a3fb44fee
davidzebua <davidzebuaa@gmail.com>
2023-09-04 20:19:52 -0600

    first-commit

---
+++ belajar-leaflet/components/atoms/Map.tsx
@@ -0,0 +1,24 @@
+// Map.tsx
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import { LatLngExpression } from 'leaflet';
+
+const Map: React.FC = () => {
+  const position: LatLngExpression = [51.50, -0.09];
+
+  return (
+    <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{ width: '500px', height: '500px' }}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default Map;
